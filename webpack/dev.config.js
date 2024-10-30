b9f84c20e406de9b3f699b00971b4f0e51a4e3e3
jimao <shihao.gu@perfma.com>
2024-10-30 18:15:23 +0800

    dev-server

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -3,4 +3,10 @@ import baseConfig from './base.config.js';
 
 export default merge(baseConfig, {
   mode: 'development',
+  devServer: {
+    open: true,
+    hot: true,
+    host: '0.0.0.0',
+    historyApiFallback: true,
+  },
 });
