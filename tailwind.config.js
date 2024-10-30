a5ecb1a66422583c832b4e50a6db7e5879b4e3ce
jimao <shihao.gu@perfma.com>
2024-10-30 23:36:53 +0800

    tailwind配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
