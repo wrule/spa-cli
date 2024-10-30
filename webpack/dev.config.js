beaddbf90358543c4f18a9ecc244796d82a5333d
jimao <shihao.gu@perfma.com>
2024-10-30 18:11:29 +0800

    绝了

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -3,9 +3,4 @@ import baseConfig from './base.config.js';
 
 export default merge(baseConfig, {
   mode: 'development',
-  devServer: {
-    open: true,
-    hot: true,
-    compress: true,
-  },
 });
