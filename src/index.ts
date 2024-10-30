65f057de7a22093e80ad46f76590e4b251e0e732
jimao <shihao.gu@perfma.com>
2024-10-30 16:15:09 +0800

    index.ts

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+
+async function main() {
+  const a: number = 1;
+  const b: number = 3;
+  console.log(a + b);
+}
+
+main();
--- webpack/base.config.js
+++ webpack/base.config.js
@@ -2,7 +2,7 @@ import { myPath, srcPath } from '../utils/myPath.js';
 
 export default {
   mode: 'production',
-  entry: srcPath('index.js'),
+  entry: srcPath('index.ts'),
   output: {
     path: myPath('dist'),
     filename: 'index.js'
