39d15c7eda11b35aaa83a62c415b30480dbf1d76
jimao <shihao.gu@perfma.com>
2024-10-30 22:18:38 +0800

    app

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+
+export default
+function App() {
+  return <span>应用界面</span>;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
 
 async function main() {
   createRoot(document.getElementById('root')!)
     .render(
       <StrictMode>
-        <span>1234</span>
+        <App />
       </StrictMode>,
     );
 }
