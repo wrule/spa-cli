a98b30c2364d824e92744fb7d5f71f43a73cc5fe
jimao <shihao.gu@perfma.com>
2024-10-31 12:59:56 +0800

    prompt

--- src/prompt.tsx
+++ src/prompt.tsx
@@ -1,3 +1,8 @@
+// 我们现在开发的是一个React的SPA应用，使用Webpack5打包
+// 我们使用typescript的tsx并且以esmodule的方式编写代码，支持es2021以上
+// 你可以使用 "tailwindcss": "^3.4.14"
+// 你可以使用 "gsap": "^3.12.5"
+// 已经安装 "@gsap/react": "^2.1.1"
 import React from 'react';
 
 export default
