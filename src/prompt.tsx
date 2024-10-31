d74aade731119cbd7f7a35670f87891d91ea5b23
jimao <shihao.gu@perfma.com>
2024-10-31 13:13:05 +0800

    提交

--- src/prompt.tsx
+++ src/prompt.tsx
@@ -2,7 +2,11 @@
 // 我们使用typescript的tsx并且以esmodule的方式编写代码，支持es2021以上
 // 你可以使用 "tailwindcss": "^3.4.14"
 // 你可以使用 "gsap": "^3.12.5"
-// 已经安装 "@gsap/react": "^2.1.1"
+// 你可以使用 "@gsap/react": "^2.1.1"
+// 你可以使用 "three": "^0.137.0"
+// 你可以使用 "@react-three/fiber": "^8.2.2"
+// 你可以使用 "framer-motion": "^11.11.10"
+// 你可以使用 "framer-motion-3d": "^11.2.0"
 import React from 'react';
 
 export default
