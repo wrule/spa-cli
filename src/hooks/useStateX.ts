fd3227a9a3a258d7449e0fc570fb76270af2796e
jimao <shihao.gu@perfma.com>
2024-11-02 21:20:42 +0800

    提交

--- src/hooks/useStateX.ts
+++ src/hooks/useStateX.ts
@@ -5,8 +5,8 @@ export
 function useStateX<T>(initValue: T): [T, (updater: (draft: T) => void) => void] {
   const [state, setState] = useState<T>(initValue);
   const freezeState = freeze(state, true);
-  const setImmerState = (updater: (draft: T) => void) => {
+  const setImmerState = useCallback((updater: (draft: T) => void) => {
     setState(produce(state, updater));
-  };
+  }, [state]);
   return [freezeState, setImmerState];
 }
