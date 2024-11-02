ce4f7e431e124f282597da03f36a171982f3f895
jimao <shihao.gu@perfma.com>
2024-11-02 21:03:11 +0800

    提交代码

--- src/hooks/useStateX.ts
+++ src/hooks/useStateX.ts
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { freeze, produce } from 'immer';
 
 export
-function useStateX<T>(initValue: T) {
+function useStateX<T>(initValue: T): [T, any] {
   const [state, setState] = useState<T>(initValue);
   const freezeState = freeze(state, true);
   return [freezeState, setState];
