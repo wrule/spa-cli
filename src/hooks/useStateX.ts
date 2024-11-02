a081635b75eb1dc69e83e69daf4fccd2bc029f74
jimao <shihao.gu@perfma.com>
2024-11-02 21:37:44 +0800

    useCallback版本

--- src/hooks/useStateX.ts
+++ src/hooks/useStateX.ts
@@ -5,11 +5,11 @@ export
 function useStateX<T>(initValue: T): [T, (updater: (draft: T) => void) => void] {
   const [state, setState] = useState<T>(initValue);
   const freezeState = freeze(state, true);
-  const setImmerState = (updater: (draft: T) => void) => {
+  const setImmerState = useCallback((updater: (draft: T) => void) => {
     const nextState = produce(state, updater);
     if (nextState !== state) {
       setState(nextState);
     }
-  };
+  }, [state]);
   return [freezeState, setImmerState];
 }
