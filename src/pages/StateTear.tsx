ff4af2ed3c2948c8c85649a7b255e2209ea811e6
jimao <shihao.gu@perfma.com>
2024-11-03 01:20:25 +0800

    提交

--- src/pages/StateTear.tsx
+++ src/pages/StateTear.tsx
@@ -2,45 +2,33 @@ import { useState, useTransition } from 'react';
 
 let bgColor = 'bg-blue-500';
 
-const data = Array(50000).fill(0).map(() => Math.random().toString());
+function getBgColor() {
+  return bgColor;
+}
+
+function setBgColor(color: string) {
+  bgColor = color;
+}
 
 export default
 function StateTear() {
   const [list, setList] = useState<string[]>([]);
   const [loading, startTransition] = useTransition();
 
-  const search1 = (keyword: string) => {
-    setList(() => {
-      console.log('计算开始 🚀', keyword);
-      if (!keyword) return [];
-      const result = data.filter((num) => num.includes(keyword));
-      console.log('计算结束 👌🏻', keyword);
-      return result;
-    });
-  };
-
-  const search2 = (keyword: string) => {
-    startTransition(() => {
-      search1(keyword);
-    });
-  };
-
   return <div>
     <div>
-      <input
-        className="border-2"
-        onChange={(event) => {
-          search2(event.target.value.trim());
-        }}
-      />
       <button onClick={() => {
-        bgColor = 'bg-red-500';
-        console.log(bgColor);
-      }}>点我</button>
+        startTransition(() => {
+          setList(Array(200000).fill(null));
+        });
+      }}>渲染列表</button>
+      <button onClick={() => {
+        setBgColor('bg-red-500');
+      }}>修改颜色</button>
     </div>
     <div>
       {loading ? <span>加载中...</span> : <ul>
-        {list.map((num, index) => <li key={index}>{num}</li>)}
+        {list.map((_, index) => <li key={index} className={(() => getBgColor())()}>{index}</li>)}
       </ul>}
     </div>
   </div>;
