c64f9212290ef28c12112fe17e865fec267eb1ed
jimao <shihao.gu@perfma.com>
2024-11-02 21:31:55 +0800

    错误方式运行?

--- src/hooks/useStateX.ts
+++ src/hooks/useStateX.ts
@@ -5,8 +5,11 @@ export
 function useStateX<T>(initValue: T): [T, (updater: (draft: T) => void) => void] {
   const [state, setState] = useState<T>(initValue);
   const freezeState = freeze(state, true);
-  const setImmerState = useCallback((updater: (draft: T) => void) => {
-    setState(produce(state, updater));
-  }, [state]);
+  const setImmerState = (updater: (draft: T) => void) => {
+    const nextState = produce(state, updater);
+    if (nextState !== state) {
+      setState(nextState);
+    }
+  };
   return [freezeState, setImmerState];
 }
