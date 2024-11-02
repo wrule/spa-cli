435a54958724b1994644e0dcda4a3f9f57a53c21
jimao <shihao.gu@perfma.com>
2024-11-02 21:18:02 +0800

    干掉不需要的事件

--- src/pages/Jotai.tsx
+++ src/pages/Jotai.tsx
@@ -18,8 +18,6 @@ export default function Jotai() {
     <div className="flex items-center justify-center w-full max-w-md gap-2 p-4">
       <input
         type="text"
-        value={searchValue.kwd}
-        onChange={(event) => setSearchValue({ kwd: event.target.value })}
         placeholder="Search..."
         className="w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200 transition-all duration-200"
       />
