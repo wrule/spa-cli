e7fe16f760bda9c29c271ef7e7557723143b1634
jimao <shihao.gu@perfma.com>
2024-11-03 01:52:08 +0800

    成功撕裂

--- src/pages/StateTear.tsx
+++ src/pages/StateTear.tsx
@@ -10,6 +10,11 @@ function setBgColor(color: string) {
   localStorage.bgColor = color;
 }
 
+function Li(props: { index: number }) {
+  const bgColor = getBgColor();
+  return <li className={bgColor}>{props.index}</li>;
+}
+
 export default
 function StateTear() {
   const [list, setList] = useState<string[]>([]);
@@ -19,7 +24,7 @@ function StateTear() {
     <div>
       <button onClick={() => {
         startTransition(() => {
-          setList(Array(200000).fill(null));
+          setList(Array(100000).fill(null));
         });
       }}>渲染列表</button>
       <button onClick={() => {
@@ -28,10 +33,7 @@ function StateTear() {
     </div>
     <div>
       {loading ? <span>加载中...</span> : <ul>
-        {list.map((_, index) => {
-          const bgColor = getBgColor();
-          return <li key={index} className={bgColor}>{index}</li>;
-        })}
+        {list.map((_, index) => <Li key={index} index={index} />)}
       </ul>}
     </div>
   </div>;
