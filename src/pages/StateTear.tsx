b6437086b05f75b9ca78e008dc76f24a1615a072
jimao <shihao.gu@perfma.com>
2024-11-02 23:16:57 +0800

    提交

---
+++ src/pages/StateTear.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useTransition } from 'react';
+
+const data = Array(100000).fill(0).map(() => Math.random().toString());
+
+export default
+function StateTear() {
+  const [list, setList] = useState<string[]>([]);
+  const [loading, startTransition] = useTransition();
+
+  const search1 = (keyword: string) => {
+    setList(() => {
+      console.log('计算开始 🚀');
+      if (!keyword) return [];
+      const result = data.filter((num) => num.includes(keyword));
+      console.log('计算结束 👌🏻');
+      return result;
+    });
+  };
+
+  const search2 = (keyword: string) => {
+    startTransition(() => {
+      search1(keyword);
+    });
+  };
+
+  return <div>
+    <div>
+      <input
+        className="border-2"
+        onChange={(event) => {
+          search2(event.target.value.trim());
+        }}
+      />
+    </div>
+    <div>
+      {loading ? <span>加载中...</span> : <ul>
+        {list.map((num, index) => <li key={index}>{num}</li>)}
+      </ul>}
+    </div>
+  </div>;
+}
