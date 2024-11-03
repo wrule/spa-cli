68c370a549b23796df27c6d2d3830204397691f9
jimao <shihao.gu@perfma.com>
2024-11-03 23:27:47 +0800

    理解了Helmet

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,5 +1,15 @@
+import { Helmet } from 'react-helmet-async';
 
 export default
 function List() {
-  return <div>列表</div>;
+  return (
+    <>
+      <Helmet>
+        <title>这里是一个列表页</title>
+      </Helmet>
+      <div>
+        <div>列表内容</div>
+      </div>
+    </>
+  );
 }
