291f1d6fe9e660e9c6ca4a31f67c09fe9fc6b295
jimao <shihao.gu@perfma.com>
2024-11-03 22:29:57 +0800

    mainLayout

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,12 @@
+import { memo } from 'react';
+import { Outlet } from 'react-router-dom';
+
+const MainLayout = () => {
+  return (
+    <main>
+      <Outlet />
+    </main>
+  );
+};
+
+export default memo(MainLayout);
