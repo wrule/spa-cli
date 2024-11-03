7767855030f4f33ec29f8a62fd998a8338aff0a5
jimao <shihao.gu@perfma.com>
2024-11-03 22:20:15 +0800

    whyDidYouRender

--- src/wdyr.tsx
+++ src/wdyr.tsx
@@ -4,6 +4,10 @@ import React from 'react';
 if (process.env.NODE_ENV === 'development') {
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
   whyDidYouRender(React, {
+    onlyLogs: true,
+    titleColor: 'green',
+    diffNameColor: 'darkturquoise',
+    trackHooks: true,
     trackAllPureComponents: true,
   });
 }
