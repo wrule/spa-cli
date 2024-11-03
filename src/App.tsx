c66ac009e823cfa2f34cc4a5c47f91dfa8c90373
jimao <shihao.gu@perfma.com>
2024-11-03 10:36:59 +0800

    提交

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Jotai from './pages/Jotai';
 import StateTear from './pages/StateTear';
+import Search from './pages/Search';
 
 export default
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/jotai" element={<Jotai />} />
       <Route path="/tear" element={<StateTear />} />
+      <Route path="/search" element={<Search />} />
     </Routes>
   </BrowserRouter>
 }
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useTransition } from 'react';
 
 const data = Array(30000).fill(null).map(() => Math.random().toString());
 
@@ -6,16 +6,20 @@ export default
 function Search() {
   const [filter, setFilter] = useState({ search: '' });
   const [list, setList] = useState<string[]>([]);
+  const [loading, startTransition] = useTransition();
 
   return <div>
     <div>
-      <input />
-      <button>Search</button>
+      <input onChange={(event) => {
+        startTransition(() => {
+          setList(() => data.fi)
+        });
+      }} />
     </div>
     <div>
-      <ul>
+      {loading ? <span>加载中...</span> : <ul>
         {list.map((_, index) => <Item key={index} value={index} />)}
-      </ul>
+      </ul>}
     </div>
   </div>;
 }
