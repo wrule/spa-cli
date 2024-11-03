5936a9ff9eab67afdfe38a32426934b295f6a756
jimao <shihao.gu@perfma.com>
2024-11-03 11:09:52 +0800

    提交

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -4,17 +4,32 @@ const data = Array(30000).fill(null).map(() => Math.random().toString());
 
 export default
 function Search() {
+  console.log('🐤 渲染了');
+
   const [filter, setFilter] = useState({ search: '' });
   const [list, setList] = useState<string[]>([]);
   const [loading, startTransition] = useTransition();
 
-  return <div>
+  let inputValue = '';
+
+  return <div className="p-4">
     <div>
-      <input onChange={(event) => {
-        startTransition(() => {
-          setList(() => data.fi)
-        });
-      }} />
+      <input
+        className="border-solid border-2 rounded-md px-2 py-0.5 border-blue-500"
+        onChange={(event) => {
+          inputValue = event.target.value.trim();
+        }}
+      />
+      <button
+        className="bg-blue-500 text-white rounded-md px-2 py-1 mx-1"
+        onClick={() => {
+          setFilter({ search: inputValue });
+        }}>
+        搜索
+      </button>
+      <button className="bg-yellow-500 text-white rounded-md px-2 py-1 mx-1">
+        切换颜色
+      </button>
     </div>
     <div>
       {loading ? <span>加载中...</span> : <ul>
