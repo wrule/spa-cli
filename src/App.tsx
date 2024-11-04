602f19d155e5fe376c2fa4225fef86d602156b73
jimao <shihao.gu@perfma.com>
2024-11-04 20:38:36 +0800

    mui的主题

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import './App.css';
 import { BrowserRouter, useRoutes } from 'react-router-dom';
+import { createTheme, StyledEngineProvider, ThemeProvider } from '@mui/material/styles';
 import { HelmetProvider } from 'react-helmet-async';
 import routes from '@/routes';
 import { Button } from '@mui/material';
+import { useMemo, useState } from 'react';
 
 export
 function AppRoutes() {
+  const [mode, setMode] = useState<'light' | 'dark'>('light');
   const routing = useRoutes(routes);
+
+  const theme = useMemo(() => createTheme({
+    palette: { mode },
+  }), [mode]);
+
   return (
-    <HelmetProvider>{routing}</HelmetProvider>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <HelmetProvider>{routing}</HelmetProvider>
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 }
 
