562c3c5548b3031fa6a2870a574482a383b886ba
jimao <shihao.gu@perfma.com>
2024-11-04 20:40:12 +0800

    主题切换成功

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import bubu from '@/assets/images/cover-1.png';
+import { Button } from '@mui/material';
 
 export default
 function Home() {
   return <div className="bg-red-400">
     <div>主页</div>
     <img src={bubu} className="w-40 h-40" />
+    <Button variant="contained">点我</Button>
   </div>;
 }
