0d4fc85333575792a6287bf01bab1558a23a580a
jimao <shihao.gu@perfma.com>
2024-11-04 13:03:14 +0800

    最小化css

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,6 +1,20 @@
 import { merge } from 'webpack-merge';
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
+import * as lightningcss from 'lightningcss';
+import browserslist from 'browserslist';
 import baseConfig from './base.config.js';
 
 export default merge(baseConfig, {
   mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin({
+        minify: CssMinimizerPlugin.lightningCssMinify,
+        minimizerOptions: {
+          targets: lightningcss.browserslistToTargets(browserslist('>= 0.25%')),
+        },
+      }),
+    ],
+  },
 });
