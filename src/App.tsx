68ac220deb0237d07809e16b193b312677f84235
jimao <shihao.gu@perfma.com>
2024-11-04 14:46:49 +0800

    提交

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ import { Button } from '@mui/material';
 
 export
 function AppRoutes() {
-  const routing = useRoutes(routes);
+  const routing =  useRoutes(routes); 
   return (
+
     <HelmetProvider>{routing}</HelmetProvider>
   );
 }
