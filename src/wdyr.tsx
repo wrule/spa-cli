66c1ae5201786124314777ccf935202ba7b9d008
jimao <shihao.gu@perfma.com>
2024-11-04 14:04:26 +0800

    wdyr配置

--- src/wdyr.tsx
+++ src/wdyr.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 
 if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
   whyDidYouRender(React, {
     onlyLogs: true,
