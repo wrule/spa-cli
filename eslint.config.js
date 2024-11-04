3e59f6d0505645f025e6b8c66e3c1ac8e0b76481
jimao <shihao.gu@perfma.com>
2024-11-04 14:46:24 +0800

    ai的配置

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
@@ -24,6 +23,11 @@ export default [
   {
     rules: {
       'react/react-in-jsx-scope': 'off',
+      'no-multi-spaces': 'error', // 禁止多个空格
+      'indent': ['error', 2], // 缩进必须是2个空格
+      'no-trailing-spaces': 'error', // 禁止行尾空格
+      'space-infix-ops': 'error', // 操作符周围要有空格
+      'comma-spacing': ['error', { 'before': false, 'after': true }], // 逗号后面要有空格
     },
     settings: {
       react: {
