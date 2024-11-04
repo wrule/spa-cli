521d5f73191a2616e2318829db512e3547d493c2
jimao <shihao.gu@perfma.com>
2024-11-04 20:53:49 +0800

    切换主题

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,17 @@
 import bubu from '@/assets/images/cover-1.png';
+import ColorModeContext from '@/context/colorMode';
 import { Button } from '@mui/material';
+import { useContext } from 'react';
 
 export default
 function Home() {
+  const colorMode = useContext(ColorModeContext);
+
   return <div className="bg-red-400">
     <div>主页</div>
     <img src={bubu} className="w-40 h-40" />
-    <Button variant="contained">点我</Button>
+    <Button variant="contained" onClick={() => {
+      colorMode.toggleColorMode();
+    }}>点我切换主题</Button>
   </div>;
 }
