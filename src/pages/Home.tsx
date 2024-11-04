741e67cf98eb61e2225492f7a89ba2ae820557b1
jimao <shihao.gu@perfma.com>
2024-11-04 13:34:19 +0800

    导入图片

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,9 @@
+import bubu from '@/assets/images/cover-1.png';
 
 export default
 function Home() {
-  return <div className="bg-red-400">主页</div>;
+  return <div className="bg-red-400">
+    <div>主页</div>
+    <img src={bubu} className="w-40 h-40" />
+  </div>;
 }
