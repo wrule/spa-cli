54f23f52f330885c26ba263b14dec374457067bc
jimao <shihao.gu@perfma.com>
2024-11-04 14:28:36 +0800

    这是我的eslint配置

--- eslint.config.js
+++ eslint.config.js
@@ -7,11 +7,13 @@ import pluginReact from "eslint-plugin-react";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     ignores: [
       'node_modules/',
       'dist/',
     ],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
   },
   {
     languageOptions: { globals: globals.browser },
