9d4d36c7bb51fd7e9829133385b89af748f87cc3
jimao <shihao.gu@perfma.com>
2024-11-05 16:41:07 +0800

    覆盖率至表皮

--- jest.config.ts
+++ jest.config.ts
@@ -42,7 +42,14 @@ const config: Config = {
   // ],
 
   // An object that configures minimum threshold enforcement for coverage results
-  // coverageThreshold: undefined,
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
 
   // A path to a custom dependency extractor
   // dependencyExtractor: undefined,
