606091b10e29b7df66c3807f2cb663d19faa45ae
jimao <shihao.gu@perfma.com>
2024-11-05 10:56:26 +0800

    提交

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import './wdyr';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '@/App';
+import { Button } from '@mui/material';
 
 async function main() {
   createRoot(document.getElementById('root')!)
