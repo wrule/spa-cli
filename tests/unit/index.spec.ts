9fa71be4523daf1ae1f107073b187d3ecd38ee17
jimao <shihao.gu@perfma.com>
2024-11-05 16:25:37 +0800

    提交

--- tests/unit/index.spec.ts
+++ tests/unit/index.spec.ts
@@ -3,6 +3,6 @@ import { twoSum } from '../../src/utils/index';
 describe('twoSum的测试', () => {
   it('测试正常情况', () => {
     const a = twoSum([1, 2, 3, 4, 5], 7);
-    expect(a).toEqual([2, 5]);
+    expect(a).toEqual([3, 2]);
   });
 });
