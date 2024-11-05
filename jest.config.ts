98bcb79348b236d1039a25c28b4983c6d59efdc1
jimao <shihao.gu@perfma.com>
2024-11-05 15:36:25 +0800

    已经可以正确的识别

--- jest.config.ts
+++ jest.config.ts
@@ -126,7 +126,7 @@ const config: Config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ['setupTests.ts'],
+  setupFilesAfterEnv: ['./setupTests.ts'],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
